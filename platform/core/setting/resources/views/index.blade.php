fc150499a97571792e85178a45eacbb7a1aa638c
samankassou <samankassoufoulla@gmail.com>
2022-03-22 13:01:14 +0100

    Remove some alerts

--- platform/core/setting/resources/views/index.blade.php
+++ platform/core/setting/resources/views/index.blade.php
@@ -1,12 +1,7 @@
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
 @section('content')
     <div id="main-settings">
-        <license-component
-            verify-url="{{ route('settings.license.verify') }}"
-            activate-license-url="{{ route('settings.license.activate') }}"
-            deactivate-license-url="{{ route('settings.license.deactivate') }}"
-            reset-license-url="{{ route('settings.license.reset') }}"
-        ></license-component>
+        
     </div>
     {!! Form::open(['route' => ['settings.edit']]) !!}
         <div class="max-width-1200">
